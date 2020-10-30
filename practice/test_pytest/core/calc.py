24e5c494c1964c6185acc49ce4e7d3b35fb7c0c5
tangjy155 <1550659823@qq.com>
2020-10-30 22:10:18 +0800

    # test_calc.py 还待处理

---
+++ practice/test_pytest/core/calc.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/10/30 20:17
+# @Author   : tangjy
+# @File     : calc.py
+
+
+class Calc:
+
+    def div(self, a, b):
+        return a/b
+
+    def mul(self, a, b):
+        return a*b
+
+
+if __name__ == "__main__":
+    pass
