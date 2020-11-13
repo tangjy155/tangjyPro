86b81439db8e79e9b750ef2552911a5991aae86b
tangjy155 <1550659823@qq.com>
2020-11-13 21:30:28 +0800

    # Appium 微信移动app，删除成员，滑动查找失败，不滑动

--- practice/appium_wx/page/search_contact_page.py
+++ practice/appium_wx/page/search_contact_page.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from practice.appium_wx.page.base_page import BasePge
-from practice.appium_wx.page.contact_details_page import ContactDetailsPage
+from practice.appium_wx.page.contact_info_page import ContactInfoPage
 
 
 class SearchContactPage(BasePge):
@@ -15,31 +15,38 @@ class SearchContactPage(BasePge):
         self._driver.find_element(MobileBy.ID, "com.tencent.wework:id/hv3").click()
 
     def get_contacts_num(self, name):
-        self._driver.find_element(MobileBy.XPATH, "//*[@text='搜索']").send_keys(name)
+        # 搜索输入框
+        self._driver.find_element(MobileBy.ID, "com.tencent.wework:id/gfs").send_keys(name)
 
-        WebDriverWait(self._driver, 10, 0.5).until(
-            expected_conditions.visibility_of_element_located((MobileBy.ID, "com.tencent.wework:id/d9v")))
+        # 搜索列表为空，会抛异常
+        # WebDriverWait(self._driver, 10, 0.5).until(
+        #     expected_conditions.visibility_of_element_located((MobileBy.ID, "com.tencent.wework:id/d9v")))
         print(f'name:{name}')
 
-        list = self._driver.find_elements(MobileBy.ANDROID_UIAUTOMATOR,
-                                 'new UiScrollable(new UiSelector()'
-                                 '.scrollable(true).instance(0))'
-                                 '.scrollIntoView(new UiSelector()'
-                                 '.textContains(\"" + name + "\").instance(0));')
+        # list = self._driver.find_elements(MobileBy.ANDROID_UIAUTOMATOR,
+        #                          'new UiScrollable(new UiSelector()'
+        #                          '.scrollable(true).instance(0))'
+        #                          '.scrollIntoView(new UiSelector()'
+        #                          '.text(\"" + name + "\").instance(0));')
+        list = self._driver.find_elements(MobileBy.ID, "com.tencent.wework:id/djf")
         print(f"list:{list}")
         return len(list)
 
     def goto_contact(self, name):
-        self._driver.find_element(MobileBy.XPATH, "//*[@text='搜索']").send_keys(name)
+        # 搜索输入框
+        self._driver.find_element(MobileBy.ID, "com.tencent.wework:id/gfs").send_keys(name)
+        # 搜索列表为空，会抛异常
+        # WebDriverWait(self._driver, 20, 0.5).until(
+        #     expected_conditions.visibility_of_element_located((MobileBy.ID, "com.tencent.wework:id/d9v")))
 
-        WebDriverWait(self._driver, 10, 0.5).until(
-            expected_conditions.visibility_of_element_located((MobileBy.ID, "com.tencent.wework:id/d9v")))
+        # self._driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+        #                            'new UiScrollable(new UiSelector()'
+        #                            '.scrollable(true).instance(0))'
+        #                            '.scrollIntoView(new UiSelector()'
+        #                            '.text(\"" + name + "\").instance(0));').click()
+        elem = self._driver.find_element(MobileBy.ID, "com.tencent.wework:id/djf")
+        print("找到一个成员")
+        elem.click()
 
-        self._driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
-                                   'new UiScrollable(new UiSelector()'
-                                   '.scrollable(true).instance(0))'
-                                   '.scrollIntoView(new UiSelector()'
-                                   '.textContains(\"" + name + "\").instance(0));').click()
-
-        return ContactDetailsPage(self._driver)
+        return ContactInfoPage(self._driver)
 
--- practice/appium_wx/testcase/test_delete_contact.py
+++ practice/appium_wx/testcase/test_delete_contact.py
@@ -14,18 +14,16 @@ class TestDeleteContact:
         contacts = self.main_page.click_contact()
 
         search_page = contacts.click_search_contact()
-        num_menber1 = search_page.get_contacts_num(name)
-        print(f"num_menber1:{num_menber1}")
+        num_member1 = search_page.get_contacts_num(name)
+
+        assert num_member1 > 0
+
+        print(f"num_member1:{num_member1}")
 
         contact_info = search_page.goto_contact(name)
         contact_info.goto_details().click_edit().delete()
-        contact_info.father_page()
-        search_page.father_page()
 
-        search_page = contacts.click_search_contact()
-        num_menber2 = search_page.get_contacts_num(name)
-        search_page.father_page()
-        print(f"num_menber2:{num_menber2}")
+        num_member2 = search_page.get_contacts_num(name)
+        print(f"num_member2:{num_member2}")
 
-
-        assert num_menber1 > num_menber2
+        assert num_member1 > num_member2
